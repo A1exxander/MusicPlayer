ee56a7d15373a964fd8259a2e2e518988788d777
A1exxander <88210134+A1exxander@users.noreply.github.com>
2022-06-14 18:34:59 -0400

    Update MusicPlayer4000.cpp

--- MusicPlayer4000.cpp
+++ MusicPlayer4000.cpp
@@ -2,7 +2,7 @@
 #include <thread>
 #include <Windows.h>
 #include <mmsystem.h>
-#include <vector>
+#include <map>
 #include <filesystem>
 #include <fstream>
 #include <queue>
@@ -17,7 +17,8 @@ class MusicPlayer4000 {
 private:
 
 	bool keyboardInUse{ false };
-	std::vector<Song> library;
+	std::map<std::string, Song> library;
+	std::string folderPath{ "C:\\MusicPlayer4000\\" };
 	std::queue<Song> songQueue;
 
 public:
@@ -28,27 +29,34 @@ public:
 
 	}
 
+	void printMenu() const { // Used for our main menu func OR music player where we may clear console on a seperate thread but we are still waiting for input from our previous mainmenu function running, instead of asking for another vars input, we will simply reprint the menu to remind the user 
+
+		std::cout << "\n\n1. View Library\n2. Exit\n\nChoice : ";
+
+	}
+
 	int menu() const {
 
-		while (keyboardInUse == true) { // incase we have queue song func or musicplayer func using the keyboard to keyboard
+		while (keyboardInUse == true) { // Incase we have queueSong OR musicPlayer function running on a seperate thread using the keyboard
 
 			Sleep(500);
 
 		}
-
-		std::cout << "\n\n1. View Library\n2. Exit\n\nChoice : ";
-
+		
+		printMenu();
 		int choice{};
-
 		std::cin >> choice;
 
 		return choice;
 
 	}
 
+
 	bool fileExists(const std::string& fileName) const {
+
 		std::ifstream file(fileName);
 		return file.good();
+
 	}
 
 	void createUserLibrary() {
@@ -56,7 +64,6 @@ public:
 		std::ifstream reader;
 		std::ofstream files;
 		std::string songInfo{ " " };
-
 		std::string filePath{ "C:\\MusicPlayer4000\\Songs.txt" };
 
 		if (fileExists(filePath)) {
@@ -73,23 +80,18 @@ public:
 
 				Song temp(songName, artistName, songLength);
 
-				library.push_back(temp);
+				library.insert({ temp.getSongName() + temp.getArtistName(), temp });
 
 			}
 		}
 
 		else {
 
-
 			std::filesystem::create_directories("C:\\MusicPlayer4000\\");
 			std::ifstream reader;
-
 			files.open(filePath);
 
-			std::cout << "\n\nPlease place any .WAV files you would like to add to your library into " << filePath << " and place each song name, artist name, and song length in seconds on a new line";
-
-			std::string enterTest{ " " };
-
+			printEmptyLibraryError();
 
 			while (reader >> songInfo) {
 
@@ -101,48 +103,58 @@ public:
 
 				Song temp(songName, artistName, songLength);
 
-				library.push_back(temp);
+				library.insert({ temp.getSongName() + temp.getArtistName(), temp });
 
 			}
 
 		}
+
 	}
 
+	const std::string& getFolderPath() const {
+	
+		return folderPath;
 
-	void printLibrary(const std::vector<Song>& library) const {
+	}
 
+	void printEmptyLibraryError() const {
+	
+		std::cerr << "\nNo songs in library! Add files to " << getFolderPath() << " and the song name, artist name, and song length in seconds for each song on a new line inside Songs.txt";
+		std::cin.get();
+
+	}
+
+	void printLibrary(const std::map<std::string, Song>& library) const {
+
+		int songNumber{ 1 };
 
 		if (library.size() == 0) {
 
-			std::string folderPath{ "C:\\MusicPlayer4000\\" };
-
-			std::cout << "\nNo songs in library! Add files to " << folderPath << " and each song name, artist name, and song length in seconds on a new line on Songs.txt";
+			printEmptyLibraryError();
 
 		}
 
-		for (auto i{ 0 }; i < library.size(); ++i) {
+		for (auto i{ library.begin() }; i != library.end(); ++i) {
 
-			std::cout << "\n" << i + 1 << ". Song Name : " << library[i].getSongName() << " Artist name : " << library[i].getArtistName();
+			std::cout << "\n" << songNumber << ". Song Name : " << i->second.getSongName() << " Artist name : " << i->second.getArtistName();
+			++songNumber;
 
 		}
 
 	}
-
 
 	void viewLibrary() {
 
 		printLibrary(library);
 
 		std::cout << "\n\n1. Play song\n2. Return\n\nChoice : ";
-
 		int choice{};
-
 		std::cin >> choice;
 
-		if (choice != 2) {
-
+		if (choice == 1) {
+			
 			keyboardInUse = true; // lock access to prevent menu function from being able to proceed while were in songplayer or getsongtoqueues critical section using keyboard, if it was a sequential process or didnt use keyboard we wouldnt need this but since we will return to user menu which uses keyboard & both getsongqueue and songplayer use keyboard, we must lock
-
+			
 			if (songQueue.empty()) {
 				std::thread t1(&MusicPlayer4000::songPlayer, this);
 				t1.detach();
@@ -154,7 +166,6 @@ public:
 		}
 
 	}
-
 
 	void songPlayer() {
 
@@ -170,47 +181,43 @@ public:
 			while (!songQueue.empty()) {
 
 				std::string currentSongName{ songQueue.front().getSongName() };
+				std::string folderPath{ "C:\\MusicPlayer4000\\" + currentSongName + ".wav" };
 				int currentSongTime{ songQueue.front().getSongTime() };
+				int currentElapasedTime{ 0 };
 
 				std::cout << "\nYOU ARE LISTENING TO " << currentSongName << "\n";
-				int time{ 0 };
-
 				std::cout << "F8. Repeat Song F9. Randomize Repeat F10. Skip Current Song ";
-
-				std::string folderPath{ "C:\\MusicPlayer4000\\" + currentSongName + ".wav" };
 
 				std::wstring stemp = std::wstring(folderPath.begin(), folderPath.end());
 				LPCWSTR sw = stemp.c_str();
 
 				PlaySound(sw, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 
-				while (time < currentSongTime) {
+				while (currentElapasedTime < currentSongTime) {
 
 					if (keyboardInUse == false) {
 
-						if (GetAsyncKeyState(VK_F8)) {
+						if (GetAsyncKeyState(VK_F8))
 							repeatSong = true;
-						}
-						else if (GetAsyncKeyState(VK_F9)) {
+						else if (GetAsyncKeyState(VK_F9)) 
 							randomizeSong = true;
-						}
-						else if (GetAsyncKeyState(VK_F10)) {
-							time = 99999;
-						}
+						else if (GetAsyncKeyState(VK_F10)) 
+							currentElapasedTime = 99999;
 					}
 
 					Sleep(1000);
-					++time;
+					++currentElapasedTime;
 
 				}
 
-				if (repeatSong == true) {
+				system("CLS");
+				printMenu();
+
+				if (repeatSong == true)
 					songQueue.push(songQueue.front());
-				}
 
-				else if (randomizeSong == true) {
-					songQueue.push(library[getRandomSong(library)]);
-				}
+				else if (randomizeSong == true)
+					songQueue.push(*getRandomSong(library));
 
 				songQueue.pop();
 
@@ -219,95 +226,107 @@ public:
 		}
 
 		PlaySound(NULL, 0, 0);
+		system("CLS");
+		printMenu();
 
 	}
 
-	int getRandomSong(const std::vector<Song>& library) const {
+	Song* getRandomSong(const std::map<std::string, Song>& library) const {
 
+		auto song = library.begin();
 		std::mt19937 mt;
+		unsigned int songChoice{ mt() % library.size() };
 
-		return mt() % library.size();
+		for (int i{ 0 }; i < songChoice; ++i) {
+
+			++song;
+
+		}
+
+		Song* temp = new Song(song->second.getSongName(), song->second.getArtistName(), song->second.getSongTime());
+
+		return temp;
 
 	}
-	
-	int selectSongWNumber(const std::vector<Song>& library) const {
 
-		std::cout << "\nSelect song number : ";
+	Song* selectSongWNumber(const std::map<std::string, Song>& library) const {
 
-		int songChoice;
-
+		auto song = library.begin();
+		int songChoice{};
+		std::cout << "\nEnter a song number : ";
 		std::cin >> songChoice;
 
-		while (songChoice > library.size() && songChoice != -1) {
+		while (songChoice > library.size()) {
 
-			std::cout << "\nEnter a number from 1 - " << library.size() << " or -1 to exit : ";
-
+			std::cout << "\nEnter a number from 1 - " << library.size();
 			std::cin >> songChoice;
 
 		}
-
 		--songChoice;
+		
+		for (int i{ 0 }; i < songChoice; ++i) {
 
-		return songChoice;
+			++song;
+
+		}
+
+		Song* temp = new Song(song->second.getSongName(), song->second.getArtistName(), song->second.getSongTime()); // We need to do this as we cant return ptr local object, nor can we get the addr of song->second for some reason, its not the best but its fine as we delete it after
+
+		return temp;
 
 	}
-	int selectSongWName(const std::vector<Song>& library) const {
 
-		std::string song{ " " };
+	Song* selectSongWName(std::map<std::string, Song>& library)  {
+
+		std::string songName{ " " };
+		std::string artistName{ " " };
 		std::cout << "\nEnter song name : ";
-		std::cin >> song;
+		std::cin >> songName;
+		std::cout << "\nEnter artist name : ";
+		std::cin >> artistName;
+		std::string key{ songName + artistName };
 
-		int i{ 0 };
+		Song* song{ nullptr };
 
-		while (i < library.size() && library[i].getSongName() != song) {
+		if (library.find(key) != library.end()) {
+		
+			song = &library.find(key)->second;
 
-			++i;
 		}
 
-		if (i < library.size() && library[i].getSongName() == song) {
-			return i;
-		}
-		else {
-			std::cout << "\nNo song found!";
-			return -1;
-		}
+		return song;
+
 	}
 
 	void getSongToQueue() {
-
-		std::cout << "\n1. Select song by number \n2. Select song by name \n3. Return\n\nChoice : ";
-
+		
 		int choice{};
-
+		std::cout << "\n1. Select song by number \n2. Select song by name \n3. Return\n\nChoice : ";
 		std::cin >> choice;
-
-		int songChoice{};
+		Song* songChoice{ nullptr };
 
 		switch (choice) {
 
-		case 1: {
-
-			songChoice = selectSongWNumber(library);
-			break;
-
-		}
-		case 2: {
-
-			songChoice = selectSongWName(library);
-			break;
-		}
-
-		case 3: {
-
-			songChoice = -1;
+			case 1: {
+				songChoice = selectSongWNumber(library);
+				break;
+			}
+			case 2: {
+				songChoice = selectSongWName(library);
+				break;
+			}
+			default: {
+				songChoice = nullptr;
+				break;
+			}
 
 		}
 
-		}
+		if (songChoice != nullptr) {
 
-		if (songChoice >= 0) {
-
-			songQueue.push(library[songChoice]);
+			songQueue.push(*songChoice);
+			delete songChoice;
+			songChoice = nullptr;
 
 		}
 
@@ -334,12 +353,13 @@ int main()
 
 		switch (m.menu()) {
 
-		case 1:
+		case 1: {
 			m.viewLibrary();
 			break;
-		default:
+		}
+		default: {
 			repeat = false;
-
+		}
 		}
 
 	}
@@ -350,6 +370,5 @@ int main()
 
 
 /* TODO -
-* Add songs to hashmap, where key is SongName + ArtistName, to make it faster to find?
 * Add GUI?
 */
